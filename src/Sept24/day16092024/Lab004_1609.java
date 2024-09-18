f3b4ba1fca36c2f6ea248b85cea5d16d11e605d1
nikunj-r-patel <nikunj.r.p.88@gmail.com>
2024-09-18 07:01:35 +0530

    code 16-09

---
+++ src/Sept24/day16092024/Lab004_1609.java
@@ -0,0 +1,33 @@
+package Sept24.day16092024;
+
+public class Lab004_1609 {
+    public static void main(String[] args) {
+        //  Compound Assignment Operators
+        int a = 10;
+        // += ,-= , *=, /=
+//        a += 20; // a = a+ 20
+//        a -= 20; // a = a - 20
+//        a /= 10; // a = a/10
+
+        // Unary Operator
+        int a1 = +65; // + ->  unary operator
+        int b1 = -1;  // - -> unary operator
+        System.out.println(a);
+        System.out.println(b1);
+        b1 = b1+1;
+        System.out.println(b1);
+
+        // Arithmetic Operator
+// +,-,/,*
+        int a2 = 10;
+        int b2 = 2;
+        System.out.println(a2+b2); // + -> binary - 2 operands
+        System.out.println(a2-b2);
+        System.out.println(a2*b2);
+        System.out.println(a2/b2);
+
+        int age = 45;
+// =  Assignment
+// right value to variable
+    }
+}
